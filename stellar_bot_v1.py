0c6d27e7208f2c321e546d771ab8bd865d6a4566
younes3334 <younsserr8@gmail.com>
2022-06-06 20:06:44 +0100

    init commit

---
+++ stellar_bot_v1.py
@@ -0,0 +1,106 @@
+import requests
+
+from stellar_sdk.asset import Asset
+from stellar_sdk.keypair import Keypair
+from stellar_sdk.network import Network
+from stellar_sdk.operation.manage_sell_offer import ManageSellOffer
+from stellar_sdk.server import Server
+from stellar_sdk.transaction_builder import TransactionBuilder
+
+import json
+
+# Preparation
+## Configure Stellar SDK to talk to the horizon instance hosted by Stellar.org
+## To use the live network, set the hostname to 'https://horizon.stellar.org'
+server = Server(horizon_url="https://horizon-testnet.stellar.org")
+## Use test network, if you need to use public network,
+## please set it to `Network.PUBLIC_NETWORK_PASSPHRASE`
+network_passphrase = Network.TESTNET_NETWORK_PASSPHRASE
+## Max fee you're willing to pay per operation in this transaction (**in stroops**).
+base_fee = 100
+
+## Create seller account
+print("Create Seller Account:")
+seller_keys = Keypair.random()
+seller_secret_key = seller_keys.secret
+seller_public_key = seller_keys.public_key
+print(f"Seller Account Secret Key: {seller_secret_key}")
+print(f"Seller Account Public Key: {seller_public_key}")
+
+friendbot_url = "https://friendbot.stellar.org"
+
+
+response1 = requests.get(friendbot_url, params={"addr": seller_public_key})
+
+print(response1)
+
+seller_account = server.load_account(account_id=seller_public_key)
+
+awesome_asset_coin = Asset("AAC", seller_public_key)
+
+
+# create sell offer
+print("Create Sell Offer")
+
+
+lumen = Asset("XLM", issuer=None)
+
+
+amount = "10"
+price = "0.5"
+
+sell_offer_transaction = (
+    TransactionBuilder(
+        source_account=seller_account,
+        network_passphrase=network_passphrase,
+        base_fee=100,
+    )
+    .append_manage_sell_offer_op(
+        selling=awesome_asset_coin, buying=lumen, amount=amount, price=price
+    )
+    .set_timeout(30)
+    .build()
+)
+
+
+sell_offer_transaction.sign(seller_secret_key)
+sell_offer_transaction_resp = server.submit_transaction(sell_offer_transaction)
+print("Sell offer created\n")
+    
+## get offer
+print("Get Offer:")
+list_amount = []
+offers = server.offers().for_seller(seller_public_key).call()
+for offer in offers["_embedded"]["records"]:
+    print(
+        f"ID: {offer['id']}\nSeller: {offer['seller']} \
+          \nSelling: {offer['selling']['asset_code']} \
+          \nAmount: {offer['amount']}\nPrice: {offer['price']}"
+    )
+    list_amount.append((offer['amount'],offer['price']))
+    offer_id = offer["id"]
+
+print("#" * 30)
+print(list_amount)
+
+
+
+transaction = (
+        TransactionBuilder(
+            source_account=seller_account,
+            network_passphrase=Network.TESTNET_NETWORK_PASSPHRASE,
+            base_fee=100,  # too lazy to fetch :)
+        )
+        .append_manage_sell_offer_op(awesome_asset_coin, seller_public_key.public_key, 'XLM', None, 'USD', '.11')
+        .append_manage_sell_offer_op(awesome_asset_coin, seller_public_key.public_key, 'XLM', None, 'USD', '.12')
+        .append_manage_sell_offer_op(awesome_asset_coin, seller_public_key.public_key, 'XLM', None, 'USD', '.13')
+
+        .build()
+    )
+
+# Sign transaction with distributor private key.
+transaction.sign(seller_secret_key)
+
+# Submit signed transaction to Horizon.
+response = server.submit_transaction(transaction)
+print(json.dumps(response, indent=2))
---
+++ trader1.py
@@ -0,0 +1,92 @@
+from stellar_sdk import Keypair, Asset, Server, Network, TransactionBuilder
+
+# TODO: implement methods to cancel trades (as long as they haven't been filled)
+#       - to do this, I would probably need a function to query open offers and go from there to cancel them
+#
+# TODO: should an additional field with a link to the transaction on Horizon be logged? !!!IMPORTANT
+#
+class Trader:
+    _YEAR = 31536000
+
+    def __init__(self, server: Server, secret_key: str, base_asset: Asset, quote_asset: Asset):
+        self.server = server
+        self.keypair = Keypair.from_secret(secret_key)
+        self.account = server.load_account(account_id=self.keypair.public_key)
+        self.base = base_asset
+        self.quote = quote_asset
+
+    ############## Public Methods ###############
+
+    # Shorter names for _place_trade functions
+    def sell_base(self, amount_base: float, price_in_quote: float):
+        return self._place_trade_base_to_quote(amount_base, price_in_quote)
+
+
+    def buy_base(self, amount_quote: float, price_in_quote: float):
+        return self._place_trade_quote_to_base(amount_quote, price_in_quote)
+
+    
+    def sell_quote(self, amount_quote: float, price_in_quote: float):
+        return self._place_trade_quote_to_base(amount_quote, price_in_quote)
+
+
+    def buy_quote(self, amount_base: float, price_in_quote: float):
+        return self._place_trade_base_to_quote(amount_base, price_in_quote)
+
+
+    def get_base_balance(self) -> float:
+        return self._get_asset_balance(self.base)
+
+
+    def get_quote_balance(self) -> float:
+        return self._get_asset_balance(self.quote)
+
+   ############## Private Methods ###############
+
+    def _get_asset_balance(self, asset: Asset) -> float:
+        account_call = self.server.accounts().account_id(self.keypair.public_key)
+        balances = account_call.call()['balances']
+
+        for bal in balances:
+            if bal['asset_type'] == 'native':
+                if asset.is_native():
+                    return float(bal['balance'])
+
+            else:
+                if bal['asset_code'] == asset.code:
+                    return float(bal['balance'])
+
+
+    def _place_trade_base_to_quote(self, amount_base: float, price_in_quote: float):
+        transaction = TransactionBuilder(
+            source_account=self.account, 
+            network_passphrase=Network.PUBLIC_NETWORK_PASSPHRASE, 
+            base_fee=100
+        ).append_manage_sell_offer_op(
+            self.base.code, self.base.issuer, self.quote.code, self.quote.issuer, str(round(amount_base, 7)), str(round(1/price_in_quote, 7))
+        ).set_timeout(30).build()
+
+        transaction.sign(self.keypair)
+        response = self.server.submit_transaction(transaction)
+
+        # TODO: handle reponse - check for success, etc.
+        return response
+
+    
+    def _place_trade_quote_to_base(self, amount_quote: float, price_in_quote: float):
+
+        price_in_base = round(1 / price_in_quote, 7) # calculate price in base since we're selling quote for base
+
+        transaction = TransactionBuilder(
+            source_account=self.account, 
+            network_passphrase=Network.PUBLIC_NETWORK_PASSPHRASE, 
+            base_fee=100
+        ).append_manage_sell_offer_op(
+            self.quote.code, self.quote.issuer, self.base.code, self.base.issuer, str(round(amount_quote, 7)), str(price_in_base)
+        ).set_timeout(30).build()
+
+        transaction.sign(self.keypair)
+        response = self.server.submit_transaction(transaction)
+
+        # TODO: handle reponse - check for success, etc.
+        return response
